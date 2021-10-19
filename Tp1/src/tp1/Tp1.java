ebd5b1865c6f9eaea35f7ccdfb956ee3ec5d8269
Rogger-Vera <85507115+Rogger-Vera@users.noreply.github.com>
2021-10-19 12:14:42 -0300

    Add files via upload
    Subiendo tp1

---
+++ Tp1/src/tp1/Tp1.java
@@ -0,0 +1,19 @@
+
+package tp1;
+    
+import igu.Principal;
+
+
+public class Tp1 {
+
+    public static void main(String[] args) {
+        
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+        
+        
+    }
+    
+}
